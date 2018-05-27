d23951a360b5d44fc1cbd4d6f52bb138fae33486
p5351280 <p5351280@gmail.com>
2018-05-28 03:00:47 +0800

    update test2

--- HW3/input02.c
+++ HW3/input02.c
@@ -1,35 +1,22 @@
-#include<stdio.h>
-char line[140], stack[140];
-int i, valid, top;
-int main(void){
-    gets(line);
-    while(gets(line)){
-        for(top = 0, i = 0,valid=1; valid; i++){
-            if(line[i]=='('){
-                stack[top++] = '(';
-            }
-            else if(line[i]==')'){
-                if(stack[top] == '('){
-                    top--;
-                }
-                else{
-                    valid = 0;
-                }
-            }
-            else if(line[i] == '['){
-                stack[top] = '[';
-            }
-            else {
-                if(stack[top] == '['){
-                    top--;
-                }
-                else{
-                    valid = 0;
-                }
-            }
-        }
-        if(valid && !top) {printf("Yes\n");}
-        else {printf("No\n");}
+#include <stdio.h>
+
+int rev(int num){
+    int reverse=0;
+    while(num){
+        reverse *= 10;
+        reverse += num%10;
+        num /= 10;
     }
+    return reverse;
+}
+
+int main(){
+    //freopen("","r",stdin);
+    int n=10053;
+    if(n>0){
+		n = rev(n);
+	}
     return 0;
 }
+
+
